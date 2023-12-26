fba8d96baf7b78b61b513312364eda03854f6c80
Allmost77 <71704381+Allmost77@users.noreply.github.com>
2023-12-26 20:26:28 +0300

    gui1

---
+++ Main/Gui.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title('Key generator')
+
+root.geometry("400x600")
+root.resizable(False, False)
+root.iconbitmap(default="icon.ico")
+
+root.mainloop()
--- Main/Main.py
+++ Main/Main.py
@@ -1,3 +1,2 @@
+import random
 
-
-print('Hello')
