098c05aac596e2046082c368dbe612e2a95c9d24
Allmost77 <71704381+Allmost77@users.noreply.github.com>
2023-12-26 21:10:12 +0300

    generate

--- Main/Gui.py
+++ Main/Gui.py
@@ -1,11 +1,22 @@
 import tkinter as tk
+import Main
+
+def generate_key():
+    keydone = Main.generate(8)
+    keytext.config(text=keydone)
 
 root = tk.Tk()
-
 root.title('Key generator')
+
+keytext = tk.Label(root, text="")
+keytext.pack()
+
+generate_button = tk.Button(root, text="Generate Key", command=generate_key)
+generate_button.pack()
 
 root.geometry("400x600")
 root.resizable(False, False)
 root.iconbitmap(default="icon.ico")
 
 root.mainloop()
+
--- Main/Main.py
+++ Main/Main.py
@@ -1,2 +1,11 @@
 import random
+import string
 
+def generate(key):
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for i in range(key))
+    return rand_string
+
+# Пример использования
+generated_key = generate(8)
+print("Generated key:", generated_key)
