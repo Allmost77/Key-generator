8577d4a3c2ab2bda78955bffc9c7cd2d505dd813
Allmost77 <71704381+Allmost77@users.noreply.github.com>
2024-01-07 13:18:27 +0300

    0.3

--- Main/Generator.py
+++ Main/Generator.py
@@ -1,6 +1,11 @@
 import random
 import string
-def generate(key):
+def generate(key_lenght, use_digits=True, use_special_chars=False):
     letters = string.ascii_lowercase
-    rand_string = ''.join(random.choice(letters) for i in range(key))
+    if use_digits:
+        letters += string.digits
+    if use_special_chars:
+        letters += string.punctuation
+
+    rand_string = ''.join(random.choice(letters) for i in range(key_lenght))
     return rand_string
--- Main/Gui.py
+++ Main/Gui.py
@@ -1,33 +1,52 @@
 import tkinter as tk
 import Generator as gen
 import pyperclip
-def generate_key(keytext):
-    keydone = "Пароль!"
-    keytext.delete(0, tk.END)
-    keydone = gen.generate(8)
-    keytext.insert(0, keydone)
-    keytext.config(text=keydone)
+
+def generate_key(keylabel, key_length_var, num_turn_var, special_chars_turn_var):
+    key_length = int(key_length_var.get())
+    use_digits = num_turn_var.get() == '1'
+    use_special_chars = special_chars_turn_var.get() == '1'
+    keydone = gen.generate(key_length, use_digits, use_special_chars)
+    keylabel.config(text=keydone)
 
 def copy(copytext):
     copytext = copytext.cget("text")
     pyperclip.copy(text=copytext)
+
 def run_gui():
     root = tk.Tk()
     root.title('Key generator')
 
-    keytext = tk.Entry(root,)
-    keytext.pack()
+    frame = tk.Frame(root)
+    frame.pack()
 
-    generate_button = tk.Button(root, text="Generate Key", command=lambda: generate_key(keytext))
-    generate_button.pack()
+    key_length_var = tk.StringVar(value='8')
+    text_len = tk.Entry(frame, textvariable=key_length_var)
+    text_len.pack(side=tk.LEFT)
 
-    copybutton = tk.Button(root, text="Copy to clipboard", command=lambda: copy(keytext))
+    keylabel = tk.Label(root, text="")
+    keylabel.pack()
+
+    special_chars_turn_var = tk.StringVar(value='0')
+    num_turn_var = tk.StringVar(value='0')  
+
+    special_chars_turn = tk.Checkbutton(root, text="Turn Special Characters", variable=special_chars_turn_var, onvalue='1', offvalue='0')
+    special_chars_turn.pack()
+
+    num_turn = tk.Checkbutton(root, text="Turn Numbers", variable=num_turn_var, onvalue='1', offvalue='0')
+    num_turn.pack()
+
+    generate_button = tk.Button(frame, text="Generate Key", command=lambda: generate_key(keylabel, key_length_var, num_turn_var, special_chars_turn_var))
+    generate_button.pack(side=tk.LEFT)
+
+    button_len = tk.Button(root, text="Set Length", command=lambda: key_length_var.set(text_len.get()))
+    button_len.pack()
+
+    copybutton = tk.Button(root, text="Copy to clipboard", command=lambda: copy(keylabel))
     copybutton.pack()
 
     root.geometry("400x600")
     root.resizable(False, False)
-
-
 
     root.mainloop()
 
